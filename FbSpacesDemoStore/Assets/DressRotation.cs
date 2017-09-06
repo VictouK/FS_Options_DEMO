034926ecf54f465bf3c197f88f901ea5b03428f4
neeraj-bedi <neerajbedico@gmail.com>
2017-09-06 10:54:32 +0530

    Dresses Grabbing And Roatation Added

---
+++ FbSpacesDemoStore/Assets/DressRotation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class DressRotation : MonoBehaviour {
+
+	public VRTK_InteractableObject VRGrabObject;
+	private bool RotateDresses = false;
+	public GameObject ParentObject;
+	void Start (){
+		
+	}
+	
+
+	void Update () {
+		if (RotateDresses == true) {
+			ParentObject.transform.Rotate (Vector3.up * Time.deltaTime * 100f);
+		} else if(RotateDresses == false){
+			
+		}
+
+	}
+
+
+	void OnTriggerEnter(Collider coll){
+		if (coll.gameObject.tag == "Table" && VRGrabObject.enabled == true) {
+			RotateDresses = false;
+		}
+
+		if (coll.gameObject.tag == "Table" && VRGrabObject.enabled == false) {
+			RotateDresses = true;
+		}	
+	}
+
+
+	void OnTriggerStay(Collider coll){
+		if (coll.gameObject.tag == "Table" && VRGrabObject.enabled == true) {
+			RotateDresses = false;
+		}
+
+		if (coll.gameObject.tag == "Table" && VRGrabObject.enabled == false) {
+			RotateDresses = true;
+		}	
+	}
+
+
+	void OnTriggerExit(Collider coll){
+		if (coll.gameObject.tag == "Table" && VRGrabObject.enabled == true) {
+			RotateDresses = false;
+		}
+
+		if (coll.gameObject.tag == "Table" && VRGrabObject.enabled == false) {
+			RotateDresses = true;
+		}	
+	}
+}
